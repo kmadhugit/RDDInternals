cdabf4eb95b18f07b541a2911d2d32c23c90f060
Madhusudanan Kandasamy <madhusudanan@in.ibm.com>
2017-03-10 12:00:46 +0530

    removed commented code

--- src/main/scala/org/madhu/HelloTwitter.scala
+++ src/main/scala/org/madhu/HelloTwitter.scala
@@ -17,19 +17,14 @@ object HelloTwitter {
 
   def process(x : RDD[Status]) : Unit = {
 
-    //val f = x.filter(s=> ( s.getText.contains("please") || s.getText.startsWith("plz") || s.getText.startsWith("pls"))   )
     val f = x
     val langs = Array("en","ta","te")
     f.filter(f=>langs.contains(f.getLang)).
-      //filter(f=>f.getHashtagEntities.map(a => a.getText).contains("RussianTVShows")).
-      // filter(f=>f.getText.toUpperCase.contains("SPARK")).
       foreach(y => {
       println(s"${y.getUser().getName} ${y.getLang} ==> ${y.getText} \n\n")
     })
-    /*     f.foreach(y => {
-           println(y.getUser().getName + "===>" + y.getText.map(a => if(a=='\n') ' ' else a ))
-         })*/
   }
+
   def main(args: Array[String]): Unit = {
     // Add your own Twitter API configuration stuff
     //////////////// TWITTER API CONF PART START ////////////////
@@ -56,29 +51,12 @@ object HelloTwitter {
     val authStuff = Option(twitter.getAuthorization)
 
     // Create the stream2
-     val stream = TwitterUtils.createStream(ssc, authStuff, Array("#MakeInIndia"))
+     val stream = TwitterUtils.createStream(ssc, authStuff, Array("#BVRIT"))
     //val stream = TwitterUtils.createStream(ssc, authStuff)
 
 
-    // Get HashTags
-    //val statuses = stream.filter(_.getLang == "en").map(_.getText).flatMap(_.split(" "))
-
-    /* statuses.foreachRDD((rdd, time) => {
-      val count = rdd.count()
-      if(count > 0) {
-        rdd.coalesce(1).saveAsTextFile("/home/kmadhu/twitter_data/data" + time.milliseconds.toString)
-        numTweetsCollected += count
-        println("Number of tweets collected: " + numTweetsCollected)
-        if (numTweetsCollected > 1000000) {
-          System.exit(0)
-        }
-      }
-    }) */
-
 
     stream.foreachRDD(process(_))
-    //stream.foreachRDD((rdd, time) => { rdd.map(r => { r.getUser() } ).foreach(x=>println(x)) } )
-    // println("Number of tweets collected: " + numTweetsCollected)
     ssc.start()
     ssc.awaitTermination()
   }
